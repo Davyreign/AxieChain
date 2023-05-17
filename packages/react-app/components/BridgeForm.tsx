0d87fad69b5ada76ec6547d26d0c39a137857125
Olubusayo Amowe <amoweolubusayo@gmail.com>
2023-05-17 22:52:26 +0100

    removed unused code

--- packages/react-app/components/BridgeForm.tsx
+++ packages/react-app/components/BridgeForm.tsx
@@ -173,36 +173,7 @@ const BridgeForm = () => {
         </div>
       </div>
 
-      {/* <div className="mb-8">
-        <label htmlFor="amount" className="text-sm font-medium">
-          You send
-        </label>
-        <div className="mt-1 relative rounded-md shadow-sm">
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-            <span className="text-gray-500 sm:text-sm"></span>
-          </div>
-          <input
-            type="text"
-            name="amount"
-            id="amount"
-            className="focus:ring-blue-500 focus:border-blue-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
-            placeholder="0.00"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-          <div className="absolute inset-y-0 right-0 flex items-center">
-            <input
-            type="text"
-            name="amount"
-            id="amount"
-            className="focus:ring-blue-500 focus:border-blue-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
-            placeholder="0.00"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-          </div>
-        </div>
-      </div> */}
+    
       <div className="mb-8 flex">
         <div className="mr-2">
           <label htmlFor="sendAmount" className="text-sm font-medium">
